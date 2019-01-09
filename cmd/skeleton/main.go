423f101fc301570fba34f6186b849ae4fce3ce26
cshou <cshou@google.com>
2019-01-08 23:40:11 -0800

    added http service for skeleton

--- cmd/skeleton/main.go
+++ cmd/skeleton/main.go
@@ -4,16 +4,31 @@ import (
 	"flag"
 	"fmt"
 	"net"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
 
 	"github.com/yolo3301/cnp/pkg/service/skeleton"
 	"github.com/yolo3301/cnp/pkg/streamer"
 
+	"golang.org/x/sync/errgroup"
 	"google.golang.org/grpc"
 
 	log "github.com/sirupsen/logrus"
 )
 
-var agentPort = flag.Int("agent_port", 8180, "The agent server port.")
+var (
+	httpPort     = flag.Int("http_port", 8280, "The http port.")
+	agentPort    = flag.Int("agent_port", 8180, "The agent server port.")
+	serviceName  = flag.String("service_name", "", "The service name")
+	streamerHost = flag.String("streamer_host", "", "The streamer host.")
+)
+
+func init() {
+	log.SetFormatter(&log.JSONFormatter{})
+	log.SetOutput(os.Stdout)
+}
 
 func main() {
 	flag.Parse()
@@ -23,5 +38,25 @@ func main() {
 	}
 	s := grpc.NewServer()
 	streamer.RegisterStreamerAgentServer(s, &skeleton.Agent{})
-	log.Fatal(s.Serve(lis))
+
+	if *serviceName == "" {
+		log.Fatal("service_name must be specified.")
+	}
+	if *streamerHost == "" {
+		log.Fatal("streamer_host must be specified.")
+	}
+	r := mux.NewRouter()
+	skeleton.NewService(r, *serviceName, *streamerHost)
+
+	var g errgroup.Group
+	g.Go(func() error {
+		return s.Serve(lis)
+	})
+	g.Go(func() error {
+		return http.ListenAndServe(fmt.Sprintf(":%d", *httpPort), nil)
+	})
+
+	if err := g.Wait(); err != nil {
+		log.Fatal(err)
+	}
 }
--- pkg/service/skeleton/service.go
+++ pkg/service/skeleton/service.go
@@ -1,4 +1,44 @@
 package skeleton
 
+import (
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/gorilla/mux"
+
+	log "github.com/sirupsen/logrus"
+)
+
 type Service struct {
+	redirectPrefix string
+	streamerHost   string
+}
+
+func NewService(r *mux.Router, redirectPrefix, streamerHost string) *Service {
+	s := &Service{
+		streamerHost:   streamerHost,
+		redirectPrefix: redirectPrefix,
+	}
+	sr := r.PathPrefix("/skeleton").Subrouter()
+	sr.PathPrefix("/u").HandlerFunc(s.handleUpload).Methods(http.MethodPost, http.MethodPut, http.MethodPatch)
+	sr.PathPrefix("/d").HandlerFunc(s.handleDownload).Methods(http.MethodGet)
+	http.Handle("/", r)
+	return s
+}
+
+func (s *Service) handleUpload(w http.ResponseWriter, r *http.Request) {
+	log.Infof("Received upload request %s: %s", r.Method, r.RequestURI)
+	relativePath := strings.TrimPrefix(r.RequestURI, "/skeleton/u/")
+	redirectURL := fmt.Sprintf("%s/streamer/uploads/%s/%s", s.streamerHost, s.redirectPrefix, relativePath)
+	log.Infof("Redirecting upload request to %s", redirectURL)
+	http.Redirect(w, r, redirectURL, http.StatusSeeOther)
+}
+
+func (s *Service) handleDownload(w http.ResponseWriter, r *http.Request) {
+	log.Infof("Received download request %s: %s", r.Method, r.RequestURI)
+	relativePath := strings.TrimPrefix(r.RequestURI, "/skeleton/d/")
+	redirectURL := fmt.Sprintf("%s/streamer/downloads/%s/%s", s.streamerHost, s.redirectPrefix, relativePath)
+	log.Infof("Redirecting download request to %s", redirectURL)
+	http.Redirect(w, r, redirectURL, http.StatusFound)
 }
