421609d242fc3c17d36cff147a6cf71c597eaa64
cshou <cshou@google.com>
2019-01-12 13:43:39 -0800

    added ingresses and configured svc accordingly

--- pkg/service/skeleton/service.go
+++ pkg/service/skeleton/service.go
@@ -11,16 +11,16 @@ import (
 )
 
 type Service struct {
-	redirectPrefix string
-	streamerHost   string
+	name         string
+	streamerHost string
 }
 
-func NewService(r *mux.Router, redirectPrefix, streamerHost string) *Service {
+func NewService(r *mux.Router, name, streamerHost string) *Service {
 	s := &Service{
-		streamerHost:   streamerHost,
-		redirectPrefix: redirectPrefix,
+		streamerHost: streamerHost,
+		name:         name,
 	}
-	sr := r.PathPrefix("/skeleton").Subrouter()
+	sr := r.PathPrefix("/" + name).Subrouter()
 	sr.PathPrefix("/u").HandlerFunc(s.handleUpload).Methods(http.MethodPost, http.MethodPut, http.MethodPatch)
 	sr.PathPrefix("/d").HandlerFunc(s.handleDownload).Methods(http.MethodGet)
 	http.Handle("/", r)
@@ -29,16 +29,16 @@ func NewService(r *mux.Router, redirectPrefix, streamerHost string) *Service {
 
 func (s *Service) handleUpload(w http.ResponseWriter, r *http.Request) {
 	log.Infof("Received upload request %s: %s", r.Method, r.RequestURI)
-	relativePath := strings.TrimPrefix(r.RequestURI, "/skeleton/u/")
-	redirectURL := fmt.Sprintf("%s/streamer/uploads/%s/%s", s.streamerHost, s.redirectPrefix, relativePath)
+	relativePath := strings.TrimPrefix(r.RequestURI, fmt.Sprintf("/%s/u/", s.name))
+	redirectURL := fmt.Sprintf("http://%s/streamer/uploads/%s/%s", s.streamerHost, s.name, relativePath)
 	log.Infof("Redirecting upload request to %s", redirectURL)
 	http.Redirect(w, r, redirectURL, http.StatusSeeOther)
 }
 
 func (s *Service) handleDownload(w http.ResponseWriter, r *http.Request) {
 	log.Infof("Received download request %s: %s", r.Method, r.RequestURI)
-	relativePath := strings.TrimPrefix(r.RequestURI, "/skeleton/d/")
-	redirectURL := fmt.Sprintf("%s/streamer/downloads/%s/%s", s.streamerHost, s.redirectPrefix, relativePath)
+	relativePath := strings.TrimPrefix(r.RequestURI, fmt.Sprintf("/%s/d/", s.name))
+	redirectURL := fmt.Sprintf("http://%s/streamer/downloads/%s/%s", s.streamerHost, s.name, relativePath)
 	log.Infof("Redirecting download request to %s", redirectURL)
 	http.Redirect(w, r, redirectURL, http.StatusFound)
 }
