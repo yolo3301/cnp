02008f2202fc7fda8d55f95e5d28e56622cc7074
cshou <cshou@google.com>
2019-01-14 23:35:21 -0800

    init opencensus with stackdriver exporter

--- cmd/skeleton/main.go
+++ cmd/skeleton/main.go
@@ -6,12 +6,13 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"time"
 
+	"contrib.go.opencensus.io/exporter/stackdriver"
 	"github.com/gorilla/mux"
-
 	"github.com/yolo3301/cnp/pkg/service/skeleton"
 	"github.com/yolo3301/cnp/pkg/streamer"
-
+	"go.opencensus.io/stats/view"
 	"golang.org/x/sync/errgroup"
 	"google.golang.org/grpc"
 
@@ -23,6 +24,7 @@ var (
 	agentPort    = flag.Int("agent_port", 8180, "The agent server port.")
 	serviceName  = flag.String("service_name", "", "The service name")
 	streamerHost = flag.String("streamer_host", "", "The streamer host.")
+	project      = flag.String("gcp_project", "", "Project to export OpenCensus data to StackDriver.")
 )
 
 func init() {
@@ -32,6 +34,25 @@ func init() {
 
 func main() {
 	flag.Parse()
+	if *project == "" {
+		log.Fatal("project must be specified.")
+	}
+
+	sd, err := stackdriver.NewExporter(stackdriver.Options{
+		ProjectID: *project,
+		// MetricPrefix helps uniquely identify your metrics.
+		MetricPrefix: *serviceName,
+	})
+	if err != nil {
+		log.Fatalf("Failed to create the Stackdriver exporter: %v", err)
+	}
+	// It is imperative to invoke flush before your main function exits
+	defer sd.Flush()
+
+	// Register it as a metrics exporter
+	view.RegisterExporter(sd)
+	view.SetReportingPeriod(60 * time.Second)
+
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *agentPort))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
--- cmd/streamer/main.go
+++ cmd/streamer/main.go
@@ -5,10 +5,13 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 
+	"contrib.go.opencensus.io/exporter/stackdriver"
 	"github.com/gorilla/mux"
 	"github.com/yolo3301/cnp/pkg/storage/file"
 	"github.com/yolo3301/cnp/pkg/streamer"
+	"go.opencensus.io/stats/view"
 
 	log "github.com/sirupsen/logrus"
 )
@@ -22,6 +25,7 @@ var (
 	port    = flag.Int("port", 8080, "The port.")
 	root    = flag.String("storage_root", "", "The storage root.")
 	cfgPath = flag.String("config_path", "", "The config file path.")
+	project = flag.String("gcp_project", "", "Project to export OpenCensus data to StackDriver.")
 )
 
 func main() {
@@ -32,6 +36,24 @@ func main() {
 	if *cfgPath == "" {
 		log.Fatal("config_path must be specified.")
 	}
+	if *project == "" {
+		log.Fatal("project must be specified.")
+	}
+
+	sd, err := stackdriver.NewExporter(stackdriver.Options{
+		ProjectID: *project,
+		// MetricPrefix helps uniquely identify your metrics.
+		MetricPrefix: "cnp-streamer",
+	})
+	if err != nil {
+		log.Fatalf("Failed to create the Stackdriver exporter: %v", err)
+	}
+	// It is imperative to invoke flush before your main function exits
+	defer sd.Flush()
+
+	// Register it as a metrics exporter
+	view.RegisterExporter(sd)
+	view.SetReportingPeriod(60 * time.Second)
 
 	storage, err := file.NewStorage(*root)
 	if err != nil {
